43f7ef43a4e66a9220627f71d9886be526e254a3
Elumion <dovgalyukvlad87@gmail.com>
2025-07-26 17:02:37 +0300

    create extension to render app block for selecting and downloading orders

---
+++ extensions/orders-list/assets/orders-list.js
@@ -0,0 +1,33 @@
+if (!customElements.get("order-list")) {
+  customElements.define(
+    "order-list",
+    class SectionOrderList extends HTMLElement {
+      constructor() {
+        super();
+      }
+
+      connectedCallback() {
+        this.downloadLink = this.querySelector(".orders-list__download-link");
+        this.addEventListener("click", this.handleClick);
+      }
+
+      handleClick = (event) => {
+        const target = event.target;
+
+        if (target.closest(".orders-list__order-label")) {
+          const url = new URL(this.downloadLink.getAttribute("href"));
+          const checkboxes = this.querySelectorAll(
+            ".orders-list__order-checkbox",
+          );
+          const ordersIds = [...checkboxes]
+            ?.map((el) => (el.checked ? el.dataset.id : ""))
+            .join(",");
+
+          url.searchParams.set("shop", this.dataset.shop);
+          url.searchParams.set("ordersIds", ordersIds);
+          this.downloadLink.setAttribute("href", url.toString());
+        }
+      };
+    },
+  );
+}
